93510be185c2a886eac435fc1df020c177408974
Andrii Solovienko <osobuy@gmail.com>
2016-05-24 18:43:12 +0300

    TripController

--- DriveMe.API/Controllers/TripController.cs
+++ DriveMe.API/Controllers/TripController.cs
@@ -4,10 +4,34 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
+using DriveMe.Domain.Models;
 
 namespace DriveMe.API.Controllers
 {
     public class TripController : ApiController
     {
+        public OkNegotiatedContentResult<Trip> Get()
+        {
+            throw new NotImplementedException();
+        }
+        public OkNegotiatedContentResult<Trip> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        public OkNegotiatedContentResult<Trip> Post()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OkNegotiatedContentResult<Trip> Put()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OkNegotiatedContentResult<Trip> Delete()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
