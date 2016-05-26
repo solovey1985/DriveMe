f436616200f97ac2be1d972ff8b5a5b925c08e84
Andrii Solovienko <osobuy@gmail.com>
2016-05-26 15:54:38 +0300

    fix for packages

---
+++ DriveMe.Core.Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DriveMe.Infrastructure;
+
+namespace DriveMe.Domain.Models
+{
+    public abstract class User:EntityBase
+    {
+        #region Properties
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public string Phone {get;set;}
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+        public string FavouriteAddress { get; set; }
+
+
+        #endregion
+
+        protected User(Guid id) : base(id) { }
+
+        protected User()
+        {}
+    }
+}
---
+++ DriveMe.Tests.NUnit/Domain/TripTest.cs
@@ -0,0 +1,16 @@
+﻿using DriveMe.Domain.Models;
+using NUnit.Framework;
+
+namespace DriveMe.Tests.NUnit.Domain
+{
+    [TestFixture]
+    public class TripTest
+    {
+       
+        public void CanGetTripById()
+        {
+            Trip trip = new Trip();
+           Assert.NotNull(trip);
+        }
+    }
+}
